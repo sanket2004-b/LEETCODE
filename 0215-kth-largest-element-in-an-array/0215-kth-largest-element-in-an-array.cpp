b1431ffb59b859cf091ea1cba6bd4c99f0f1fca7
sanket2004-b <sanketmarkand7676@gmail.com>
2025-07-10 11:06:09 +0530

    Time: 22 ms (83.37%), Space: 61.5 MB (44.99%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -7,18 +7,18 @@ public:
         int n=nums.size();
         // return nums[n-k];
         priority_queue<int,vector<int >,greater<int>>q;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<k;i++){
             q.push(nums[i]);
         }
-        // for(int i=k;i<n;i++){
-        //     if(q.top()<nums[i]){
-        //         q.pop();
-        //         q.push(nums[i]);
-        //     }
-        // }
-        while(q.size()>k){
-            q.pop();
+        for(int i=k;i<n;i++){
+            if(q.top()<nums[i]){
+                q.pop();
+                q.push(nums[i]);
+            }
         }
+        // while(q.size()>k){
+        //     q.pop();
+        // }
         return q.top();
     }
 };
